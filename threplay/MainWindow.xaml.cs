9ead0cd76c0883f35f318121f0046459686c3dff
raviddog <raviddog@outlook.com>
2019-03-22 16:54:55 +1100

    added replay user data reading functionality for the common format shared between touhou 10 - 16 and onwards

--- threplay/MainWindow.xaml.cs
+++ threplay/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -717,7 +718,7 @@ namespace threplay
                     break;
                 case "54385250":
                     //T8RP
-                    status = T8RP(ref replay);
+                    status = Read_T8RP(ref replay.replay);
                     break;
                 case "54395250":
                     //T9RP
@@ -727,12 +728,15 @@ namespace threplay
                     break;
                 case "74313072":
                     //t10r
+                    status = Read_t10r(ref replay.replay);
                     break;
                 case "74313172":
                     //t11r
+                    status = Read_t11r(ref replay.replay);
                     break;
                 case "74313272":
                     //t12r
+                    status = Read_t12r(ref replay.replay);
                     break;
                 case "74313235":
                     //t125
@@ -743,15 +747,19 @@ namespace threplay
                 case "74313372":
                     //t13r
                     //has both td and ddc for some fucking reason
+                    //since im reading the user data at the end though it doesnt matter
+                    status = Read_t13r(ref replay.replay);
                     break;
                 case "74313433":
                     //t143
                     break;
                 case "74313572":
                     //t15r
+                    status = Read_t15r(ref replay.replay);
                     break;
                 case "74313672":
                     //t16r
+                    status = Read_t16r(ref replay.replay);
                     break;
                 case "74313536":
                     //t156
@@ -779,7 +787,7 @@ namespace threplay
             return val;
         }
 
-        private static string ReadString()
+        private static string ReadStringANSI()
         {
             int[] buf = new int[3];
             string val = string.Empty;
@@ -794,64 +802,95 @@ namespace threplay
                     buf[0] = buf[1];
                     buf[1] = buf[2];
                     buf[2] = file.ReadByte();
-                }
-                while (buf[0] != 13 && buf[1] != 10);
+                } while (buf[0] != 13 && buf[1] != 10);
             }
+
+            file.Seek(-1, SeekOrigin.Current);
 
             return val;
         }
 
-        private static bool T8RP(ref ReplayEntry replay)
+        //attempting to read japanese characters and failing
+        private static string ReadStringWide()
         {
-            file.Seek(12, SeekOrigin.Begin);
+            int[] buf = new int[3];
+            List<char> val = new List<char>();
+
+            buf[0] = file.ReadByte();
+            buf[1] = file.ReadByte();
+            if(buf[0] != 13 && buf[1] != 10)
+            {
+                buf[2] = file.ReadByte();
+                do
+                {
+                    val.Add((char)buf[0]);
+                    buf[0] = buf[1];
+                    buf[1] = buf[2];
+                    buf[2] = file.ReadByte();
+                } while (buf[0] != 13 && buf[1] != 10);
+            }
+
+            file.Seek(-1, SeekOrigin.Current);
+
+            Encoding iso = Encoding.GetEncoding("ISO-8859-1");
+            Encoding utf16 = Encoding.Unicode;
+
+            byte[] valUTF16 = utf16.GetBytes(val.ToArray());
+            byte[] valISO = Encoding.Convert(utf16, iso, valUTF16);
+            return iso.GetString(valISO);
+        }
+
+        private static bool JumpToUser(int loc)
+        {
+            file.Seek(loc, SeekOrigin.Begin);
             UInt32 offset = ReadUInt32();
-            //get offset to user info
-
-            string val = string.Empty;
-            int buf;
-
-            //move read position to start of user data
             try { file.Seek(offset, SeekOrigin.Begin); }
             catch { return false; }
 
-            //read user magic to verify correct spot
-            for(int i = 0; i < 4; i++)
+            string val = string.Empty;
+            int buf;
+            for (int i = 0; i < 4; i++)
             {
                 buf = file.ReadByte();
                 val = string.Concat(val, string.Format("{0:X2}", buf));
             }
-            if (val != "55534552") return false;
-            val = string.Empty;
+            return val == "55534552" ? true : false;
+        }
+        
+        /*
+         * Format of the Decoding Functions for Touhou 8 - onwards
+         * 
+         * Offset to USER info is stored at offset 0xC, length 4
+         * Jump to offset, return false if exception thrown
+         * Read USER to confirm correct jump, return false if not
+         * 
+         * Store the USER data length, it may come in handy?
+         * Terminating bytes for a string are 0x0D, 0x0A
+         * Jump to and read PLAYER NAME as string
+         * Jump to and read DATE as string
+         * Jump to and read CHARACTER as string, if the replay was encoded by a japanese copy of the game this reading will be gibberish
+         *      - Implement a lookup table for each game to convert the hex into a readable string
+         * Jump to and read SCORE as long, then format it to a string with thousand separators
+         * Jump to and read DIFFICULTY as string
+         * 
+         * */
 
-            //get and store user data length, because why not
-            UInt32 length = ReadUInt32();
-
-            //move to start of player name
-            file.Seek(17, SeekOrigin.Current);
-
-            //read player name into replayinfo
-            replay.replay.name = ReadString();
-
-            //move to and read date
-            file.Seek(10, SeekOrigin.Current);
-            replay.replay.date = ReadString();
-
-            //move to and read character
-            file.Seek(8, SeekOrigin.Current);
-            replay.replay.character = ReadString();
-
-            //move to and read score
-            file.Seek(7, SeekOrigin.Current);
-            try
-            {
-                long scoreConv = long.Parse(ReadString());
-                replay.replay.score = scoreConv.ToString("N0");
-            } catch { }
+        private static bool Read_T8RP(ref ReplayEntry.ReplayInfo replay)
+        {
+            if (!JumpToUser(12)) return false;
             
-
-            //move to and read difficulty
-            file.Seek(7, SeekOrigin.Current);
-            replay.replay.difficulty = ReadString();
+            UInt32 length = ReadUInt32();
+            file.Seek(17, SeekOrigin.Current);
+            replay.name = ReadStringANSI();
+            file.Seek(11, SeekOrigin.Current);
+            replay.date = ReadStringANSI();
+            file.Seek(9, SeekOrigin.Current);
+            replay.character = ReadStringANSI();
+            file.Seek(8, SeekOrigin.Current);
+            long.TryParse(ReadStringANSI(), out long scoreConv);
+            replay.score = scoreConv.ToString("N0");
+            file.Seek(8, SeekOrigin.Current);
+            replay.difficulty = ReadStringANSI();
 
             //check if spell practice or game replay
             //actually do this later
@@ -859,6 +898,62 @@ namespace threplay
             return true;
         }
 
+        //  the replay user data format for touhou 10 through to 16 (and presumably from here on in) is identical
+
+        private static bool Read_t10r(ref ReplayEntry.ReplayInfo replay)
+        {
+            if (!JumpToUser(12)) return false;
+
+            UInt32 length = ReadUInt32();
+            file.Seek(4, SeekOrigin.Current);
+            ReadStringANSI();   //SJIS, 東方XYZ リプレイファイル情報, Touhou XYZ replay file info
+            ReadStringANSI();   //Skip over game version info
+            file.Seek(5, SeekOrigin.Current);
+            replay.name = ReadStringANSI();
+            file.Seek(5, SeekOrigin.Current);
+            replay.date = ReadStringANSI();
+            file.Seek(6, SeekOrigin.Current);
+            replay.character = ReadStringANSI();
+            file.Seek(5, SeekOrigin.Current);
+            replay.difficulty = ReadStringANSI();
+            file.Seek(6, SeekOrigin.Current);
+            ReadStringANSI();   //stage
+            file.Seek(6, SeekOrigin.Current);
+            long.TryParse(ReadStringANSI() + "0", out long scoreConv);  //replay stores the value without the 0
+            replay.score = scoreConv.ToString("N0");
+
+            return true;
+        }
+
+        private static bool Read_t11r(ref ReplayEntry.ReplayInfo replay)
+        {
+            return Read_t10r(ref replay);
+        }
+
+        private static bool Read_t12r(ref ReplayEntry.ReplayInfo replay)
+        {
+            return Read_t10r(ref replay);
+        }
+
+        private static bool Read_t13r(ref ReplayEntry.ReplayInfo replay)
+        {
+            return Read_t10r(ref replay);
+        }
+
+        private static bool Read_t14r(ref ReplayEntry.ReplayInfo replay)
+        {
+            return Read_t10r(ref replay);
+        }
+
+        private static bool Read_t15r(ref ReplayEntry.ReplayInfo replay)
+        {
+            return Read_t10r(ref replay);
+        }
+
+        private static bool Read_t16r(ref ReplayEntry.ReplayInfo replay)
+        {
+            return Read_t10r(ref replay);
+        }
     }
 
     public class ReplayEntry
